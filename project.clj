c0f8896f8c015547c8e0c4f66d1e8adb20add708
Ben Cook <b@bencook.ca>
2015-04-08 04:49:22 -0700

    Remove JS deps from project.clj now that we have a bower+package.json

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject invigilou "0.1.0-SNAPSHOT"
   :description "Analysis of UBC exam schedule"
-  :url "https://github.com/blx/exam-traffic"
+  :url "https://github.com/blx/invigilou"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
@@ -14,20 +14,7 @@
                  [clj-jade "0.1.5"]
                  [ring/ring-json "0.3.1"]]
   :plugins [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]
-            [lein-bower "0.5.1" :exclusions [org.clojure/clojure]]
-            [lein-coffee "0.2.1" :exclusions [org.clojure/clojure]]
             [lein-marginalia "0.8.0" :exclusions [org.clojure/clojure]]]
-  :bower-dependencies [[cal-heatmap "3.5.2"]
-                       [moment "2.9.0"]
-                       [EventDrops "blx/EventDrops#master"]]
-  :bower {:directory "resources/public/js/lib"}
-  :lein-coffee
-  {;:compile-hook true
-   ;:jar-hook true
-   :coffee-version "1.9.0"
-   :coffee {:sources ["src/coffee/exams.coffee"]
-            :output "resources/public/js"
-            :bare true}}
   :ring {:handler invigilou.handler/app}
   :uberjar-name "invigilou-standalone.jar"
   :profiles
