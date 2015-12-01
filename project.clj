e4badd074828d9696059eef46a8447d5f24fc5e0
Ben Cook <b@bencook.ca>
2015-12-01 04:02:42 -0800

    bump/cleanup deps, use clojure 1.7

--- project.clj
+++ project.clj
@@ -1,23 +1,22 @@
-(defproject invigilou "0.1.0-SNAPSHOT"
+(defproject invigilou "0.1.2-SNAPSHOT"
   :description "Analysis of UBC exam schedule"
   :url "https://github.com/blx/invigilou"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]
-                 [org.clojure/java.jdbc "0.3.6"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]
-                 [clj-http "1.1.0"]
-                 [enlive "1.1.5"]
-                 [clj-jade "0.1.5"]
-                 [ring/ring-json "0.3.1"]]
-  :plugins [[lein-ring "0.8.13" :exclusions [org.clojure/clojure]]
-            [lein-marginalia "0.8.0" :exclusions [org.clojure/clojure]]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [environ "1.0.1"]
+                 [org.clojure/java.jdbc "0.4.2"]
+                 [org.xerial/sqlite-jdbc "3.8.11"]
+                 [clj-http "2.0.0"]
+                 [enlive "1.1.6"]
+                 [clj-jade "0.1.7"]
+                 [ring/ring-json "0.4.0"]]
+  :plugins [[lein-ring "0.9.7" :exclusions [org.clojure/clojure]]]
   :ring {:handler invigilou.handler/app}
   :uberjar-name "invigilou-standalone.jar"
   :profiles
   {:uberjar {:aot :all}
    :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring/ring-mock "0.3.0"]]}})
